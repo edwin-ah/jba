b75d41d8a9b05da539a5cb05b029c7ae054dea9d
edwin-ah <edwin.hallsten@hotmail.com>
2020-12-23 12:20:31 +0100

    first commit

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,79 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+        <title>JBA</title>
+
+        <!-- Scripts -->
+        <script src="{{ asset('js/app.js') }}" defer></script>
+
+        <!-- Styles -->
+        <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    </head>
+    <body>
+        <!-- Navbar -->
+        <div id="nav-bar">
+            <nav class="navbar navbar-expand-md navbar-light bg-light sticky-top">
+              <div class="container-fluid">
+                <a class="navbar-brand" href="index.html">
+                  <img id="nav-logo" src="{{ URL('images/logos/jba_logo.png') }}" alt="logo">
+                </a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive">
+                  <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarResponsive">
+                  <ul class="navbar-nav ml-auto">
+                    <li class="nav-item active"> 
+                      <a class="nav-link" href="index.html">Hem</a>
+                    </li>
+                    <li class="nav-item"> 
+                      <a class="nav-link" href="#">Om Oss</a>
+                    </li>
+                    <li class="nav-item"> 
+                        <a class="nav-link" href="#">Projekt</a>
+                      </li>
+                    <li class="nav-item"> 
+                      <a class="nav-link" href="#">Kontakt</a>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            </nav>
+          </div>
+
+          @yield('content')
+
+          <!--Footer-->
+          <!--Logo, Kontakt, länkar ... utv av-->
+          <footer>
+            <div class="container-fluid padding">
+                <div class="row align-items-center text-center">
+                    <div class="col-md-4 align-baseline">
+                        <img class="footer-logo" src="{{ URL('images/logos/jba_footer_logo.png') }}" alt="logo">
+                    </div>
+                    <div class="col-md-4">
+                        <h5>Kontakt:</h5>
+                        <hr class="light">
+                        <p>0725424064</p>
+                        <p>edwin.hallsten@hotmail.com</p>
+                    </div>
+                    <div class="col-md-4">
+                        <h5>Länkar:</h5>
+                        <hr class="light">
+                        <a href="#">Om oss</a>
+                        <br/>
+                        <a href="#">Projekt</a>
+                        <br/>
+                        <a href="#">Kontakt</a>
+                    </div>
+                    <div class="col-12">
+                        <hr class="light-100">
+                        <p>Utvecklad av Edwin Andersson Hallsten<p>
+                    </div>
+                </div>
+            </div>
+          </footer>
+    </body>
+</html>
---
+++ resources/views/pages/index.blade.php
@@ -0,0 +1,38 @@
+@extends('layouts.app')
+@section('content')
+    <!--Jumbotron-->
+    <div class="jumbotron text-white jumbotron-image shadow">
+        <div class="container">
+          <h1 class="ml-5 mb-3 ">Johannes Lundebacke</h1>
+          <p class="mx-5 lead">Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil, quo quos. Accusantium magni illum exercitationem minus aperiam error dolorem doloribus recusandae consequatur! Odit, blanditiis deleniti! Id veritatis porro officiis unde! Lorem ipsum dolor sit, amet consectetur adipisicing elit. Officia cumque ipsum maiores at eaque laborum, ratione explicabo natus harum ea sed molestias repellendus nisi hic iure delectus, error commodi tenetur. Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam corrupti quos, minus asperiores libero voluptatibus molestiae voluptas porro sunt aspernatur nostrum dolorum voluptate dolores quam amet consequatur id praesentium illo?</p>
+          <hr class="my-4">
+          <a class="ml-2 btn btn-primary" href="#">Mer om oss!</a>
+          <a class="ml-2 btn btn-primary" href="#">Kontakta oss!</a>
+        </div>
+      </div>
+
+
+      <!--Ikoner: Hus(Projekt) Telefon(Kontakt) Gubbe(Om oss)-->
+      <div class="container-fluid padding">
+        <div class="row text-center padding">
+          <div class="col-xs-12 col-md-4">
+            <a href="#">
+              <img src="{{ URL('images/icons/project_icon.png') }}" alt="projekt-ikon">
+              <h4 class="icon-text">Kolla in våra projekt</h4>
+            </a>  
+          </div>
+          <div class="col-xs-12 col-md-4">
+            <a href="#">
+              <img src="{{ URL('images/icons/phone_icon.png') }}" alt="kontakt-ikon">
+              <h4 class="icon-text">Kontakta oss</h4>
+            </a>
+          </div>
+          <div class="col-xs-12 col-md-4">
+            <a href="#">
+              <img src="{{ URL('images/icons/about_icon.png') }}" alt="om_oss-ikon">
+              <h4 class="icon-text">Mer om oss</h4>
+            </a>  
+          </div>
+        </div>
+      </div>
+@endsection
